13cd331edddaf70f95cca1a51bae5b982e91033e
Fabio Manente <101602674+FabioManente@users.noreply.github.com>
2024-07-23 18:13:30 -0300

    Create app.js

---
+++ img/JS Game_files/app.js
@@ -0,0 +1,23 @@
+@@ -0,0 +1,22 @@
+let numeroSecreto = parseInt(Math.random() * 11)
+let tentativas = 1
+deixe a calha
+
+alert('Boas vindas ao jogo do número secreto')
+
+while (chute != numeroSecreto) {
+let chute = prompt('Escolha um número entre 1 e 10')
+if (chute == numeroSecreto) {
+quebrar
+} else {
+if (chute > numeroSecreto) {
+alert('O número secreto é menor')
+} else {
+alert('O número secreto é maior')
+//         }
+//     }
+tentativas++
+// }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
+alert('O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}')
